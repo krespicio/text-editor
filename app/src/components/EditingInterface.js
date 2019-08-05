e912259cf1f430971c94962e8b1ad8d2b257065b
caroline <pl5bx@virginia.edu>
2019-08-05 17:52:41 -0400

    color

--- app/src/components/EditingInterface.js
+++ app/src/components/EditingInterface.js
@@ -2,21 +2,56 @@ import React from "react";
 import ReactDOM from "react-dom";
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 // import { faCoffee } from '@fortawesome/free-solid-svg-icons'
-import { Editor, EditorState, RichUtils } from "draft-js";
+import { Editor, EditorState, RichUtils, Modifier } from "draft-js";
 
 class EditingInterface extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      editorState: EditorState.createEmpty()
+      editorState: EditorState.createEmpty(),
+      bold: false
     };
     this.onChange = editorState => this.setState({ editorState });
     this.handleKeyCommand = this.handleKeyCommand.bind(this);
+    this.toggleColor = toggledColor => this._toggleColor(toggledColor);
+    this.focus = () => this.editor.focus();
     this._onClick = e => {
       this.onChange(
         RichUtils.toggleInlineStyle(this.state.editorState, e.target.name)
       );
     };
+  }
+
+  _toggleColor(toggledColor) {
+    const { editorState } = this.state;
+    const selection = editorState.getSelection();
+    // Let's just allow one color at a time. Turn off all active colors.
+    const nextContentState = Object.keys(colorStyleMap).reduce(
+      (contentState, color) => {
+        return Modifier.removeInlineStyle(contentState, selection, color);
+      },
+      editorState.getCurrentContent()
+    );
+    let nextEditorState = EditorState.push(
+      editorState,
+      nextContentState,
+      "change-inline-style"
+    );
+    const currentStyle = editorState.getCurrentInlineStyle();
+    // Unset style override for current color.
+    if (selection.isCollapsed()) {
+      nextEditorState = currentStyle.reduce((state, color) => {
+        return RichUtils.toggleInlineStyle(state, color);
+      }, nextEditorState);
+    }
+    // If the color is being toggled on, apply it.
+    if (!currentStyle.has(toggledColor)) {
+      nextEditorState = RichUtils.toggleInlineStyle(
+        nextEditorState,
+        toggledColor
+      );
+    }
+    this.onChange(nextEditorState);
   }
 
   handleKeyCommand(command, editorState) {
@@ -29,17 +64,19 @@ class EditingInterface extends React.Component {
   }
 
   render() {
-    const buttonStyles = ["BOLD", "ITALIC", "UNDERLINE", "CODE"];
+    const textStyles = ["BOLD", "ITALIC", "UNDERLINE", "CODE"];
     return (
       <div>
         <div style={styles.toolbar}>
-          {buttonStyles.map(style => {
-            return (
-              <button key={style} onClick={this._onClick} name={style}>
-                {style}
-              </button>
-            );
-          })}
+          <ColorControls
+            editorState={this.state.editorState}
+            onToggle={this.toggleColor}
+          />
+          {textStyles.map(style => (
+            <button key={style} onClick={this._onClick.bind(this)} name={style}>
+              {style}
+            </button>
+          ))}
         </div>
 
         <div
@@ -54,6 +91,7 @@ class EditingInterface extends React.Component {
             editorState={this.state.editorState}
             onChange={this.onChange}
             handleKeyCommand={this.handleKeyCommand}
+            customStyleMap={colorStyleMap}
           />
         </div>
 
@@ -67,6 +105,81 @@ class EditingInterface extends React.Component {
   }
 }
 
+class StyleButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.onToggle = e => {
+      e.preventDefault();
+      this.props.onToggle(this.props.style);
+    };
+  }
+  render() {
+    let style;
+    if (this.props.active) {
+      style = { ...styles.styleButton, ...colorStyleMap[this.props.style] };
+    } else {
+      style = styles.styleButton;
+    }
+    return (
+      <span style={style} onMouseDown={this.onToggle}>
+        {this.props.label}
+      </span>
+    );
+  }
+}
+
+var COLORS = [
+  { label: "Red", style: "red" },
+  { label: "Orange", style: "orange" },
+  { label: "Yellow", style: "yellow" },
+  { label: "Green", style: "green" },
+  { label: "Blue", style: "blue" },
+  { label: "Indigo", style: "indigo" },
+  { label: "Violet", style: "violet" }
+];
+const ColorControls = props => {
+  var currentStyle = props.editorState.getCurrentInlineStyle();
+  return (
+    <div style={styles.controls}>
+      {COLORS.map(type => (
+        <div style={{ display: "inline-block", marginRight: "5px" }}>
+          <StyleButton
+            key={type.label}
+            active={currentStyle.has(type.style)}
+            label={type.label}
+            onToggle={props.onToggle}
+            style={type.style}
+          />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+const colorStyleMap = {
+  red: {
+    color: "rgba(255, 0, 0, 1.0)"
+  },
+  orange: {
+    color: "rgba(255, 127, 0, 1.0)"
+  },
+  yellow: {
+    color: "rgba(180, 180, 0, 1.0)"
+  },
+  green: {
+    color: "rgba(0, 180, 0, 1.0)"
+  },
+  blue: {
+    color: "rgba(0, 0, 255, 1.0)"
+  },
+  indigo: {
+    color: "rgba(75, 0, 130, 1.0)"
+  },
+  violet: {
+    color: "rgba(127, 0, 255, 1.0)"
+  }
+};
+
 const styles = {
   saveButton: {
     justifyContent: "flex-end",
@@ -78,7 +191,12 @@ const styles = {
     height: "200px",
     margin: "0 auto"
   },
-  toolbar: {}
+  controls: {
+    fontFamily: "'Helvetica', sans-serif",
+    fontSize: 14,
+    marginBottom: 10,
+    userSelect: "none"
+  }
 };
 
 export default EditingInterface;
