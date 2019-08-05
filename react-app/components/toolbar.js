06491512f4e0e416d780ff4b809173502fa7ad64
Christine <christineayoh9@gmail.com>
2019-08-05 13:45:58 -0400

    making containers and components

---
+++ react-app/components/toolbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function Toolbar (props){
+  return(
+    <div>
+      <h1> This is the toolbar </h1>
+    </div>
+  )
+}
+
+export default Toolbar;
